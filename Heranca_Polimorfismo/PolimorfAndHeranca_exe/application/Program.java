22ef3036222e8e850a185336cc44c9ffe58d33c9
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-11-26 02:34:18 -0300

    Implementando funcionalidades no prog. principal

--- Heranca_Polimorfismo/PolimorfAndHeranca_exe/application/Program.java
+++ Heranca_Polimorfismo/PolimorfAndHeranca_exe/application/Program.java
@@ -1,7 +1,13 @@
 package application;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+import utilities.Util;
 
 public class Program {
 
@@ -10,11 +16,60 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
+		System.out.print("Insira o número de funcionários: ");
+		int numberFuncionarios = sc.nextInt();
+		
+		// Lista para armazenar os Funcionarios
+		List <Employee> listaFuncionarios = new ArrayList<> (); 
+		
+		for (int i = 1; i <= numberFuncionarios; i++) {
+			
+			// Declaração do tipo Funcionario e variável resposta
+			Employee funcionario;
+			String resposta;
+			
+			// Coletando os dados do funcionário
+			System.out.println("Dados do funcionario #" + i);
+			
+			// Previne inconsistências
+			do {
+				System.out.print("Terceirizado (sim/não)? ");
+				resposta = sc.next().toLowerCase();
+			} while (resposta.charAt(0) != 'n' && resposta.charAt(0) != 's');
+			
+			sc.nextLine();
+		
+			System.out.print("Nome: ");
+			String name = sc.nextLine();
+					
+			System.out.print("Horas trabalhadas: ");
+			int hours = sc.nextInt();
+					
+			System.out.print("Valor por hora: ");
+			double valuePerHour = sc.nextDouble();
+					
+			// Decide se o tipo genérico Employee irá ou não receber um objeto específico e comportamentos de outro tipo
+			if (resposta.charAt(0) == 's') {
+				System.out.print("Despesa adicional: ");
+				double additionalCharge = sc.nextDouble();
+				funcionario = new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge);
+			}
+			else {
+				funcionario = new Employee(name, hours, valuePerHour);
+			}
+			
+			listaFuncionarios.add(funcionario); // Registrando funcionarios numa lista
+					
+		} // Final do laço for
 		
 		
+		System.out.println("PAYMENTS: ");
 		
-		sc.close();
-		
+		// Método responsável por retornar as informações dos funcionarios na lista
+		System.out.println(Util.employeeInformation(listaFuncionarios)); 
+			
+		sc.close();			
+			
 	}
 
 }

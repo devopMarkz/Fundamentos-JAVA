96f45046ee80aa2fac4a739da8fb1c69389136ba
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-11-26 13:37:33 -0300

    Implementando classe ProdutoUsado

---
+++ Heranca_Polimorfismo/PolimorfAndHeranca_exe1/entities/ProdutoUsado.java
@@ -0,0 +1,43 @@
+package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public final class ProdutoUsado extends Produto {
+	
+	private LocalDate manufactureDate;
+	
+	// Formatação de data
+	private static DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
+	// Construtores da classe
+	
+	public ProdutoUsado () {
+	}
+	
+	public ProdutoUsado (String name, double price, LocalDate manufactureDate) {
+		super (name, price);
+		this.manufactureDate = manufactureDate;
+	}
+
+	// Métodos getters and setters
+	
+	public LocalDate getManufactureDate() {
+		return manufactureDate;
+	}
+
+	public void setManufactureDate(LocalDate manufactureDate) {
+		this.manufactureDate = manufactureDate;
+	}
+	
+	// Métodos da classe
+	
+	@Override 
+	public String priceTag () {
+		return super.getName() 
+				+ " (used) "
+				+ super.getPrice()
+				+ String.format(" (Manufacture date: %s) ", manufactureDate.format(fmt1));
+	}
+
+}

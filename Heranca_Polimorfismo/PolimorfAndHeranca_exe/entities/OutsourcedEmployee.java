1fbe1463cadd91c9ab4e21b4ae68a0cd1734701e
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-11-26 00:51:45 -0300

    Aperfeiçoando classe Funcionario terceirizado

--- Heranca_Polimorfismo/PolimorfAndHeranca_exe/entities/OutsourcedEmployee.java
+++ Heranca_Polimorfismo/PolimorfAndHeranca_exe/entities/OutsourcedEmployee.java
@@ -4,5 +4,39 @@ package entities;
 public final class OutsourcedEmployee extends Employee {
 	
 	private Double aditionalCharge;
+	
+	// Construtores da classe
+	
+	public OutsourcedEmployee () {
+	}
+	
+	public OutsourcedEmployee (String name, int hours, double valuePerHour, double aditionalCharge) {
+		super (name, hours, valuePerHour);
+		this.aditionalCharge = aditionalCharge;
+	}
+	
+	// Métodos getters and setters
+
+	public Double getAditionalCharge() {
+		return aditionalCharge;
+	}
+
+	public void setAditionalCharge(Double aditionalCharge) {
+		this.aditionalCharge = aditionalCharge;
+	}
+	
+	// Métodos da classe
+	
+	@Override
+	public double payment () {
+		return super.payment() 
+				+ (this.aditionalCharge * 110 / 100);
+	}
+	
+	@Override
+	public String toString () {
+		return super.name + ", "
+				+ this.payment();
+	}
 
 }

19114bcbff607f4a107dc6e75a32abb9f8602e5f
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-12-02 19:32:13 -0300

    Criando classe Reserva no pacote entities

---
+++ Tratamento de Excecoes/Solucao ruim/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		sc.close();
+
+	}
+
+}

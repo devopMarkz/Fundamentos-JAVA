61ed6c5348aafaa3571d4664e750da45ae6d7d63
Marcos André <117694734+devopMarkz@users.noreply.github.com>
2022-12-05 21:42:44 -0300

    Tratando as exceções no programa principal

--- Tratamento de Excecoes/TratamentoDeExcecoes_exe1/application/Program.java
+++ Tratamento de Excecoes/TratamentoDeExcecoes_exe1/application/Program.java
@@ -2,65 +2,77 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Reservation;
+import model.exceptions.ReservationException;
+import model.exceptions.ReservationUpdateDatesException;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in);
-		
-		DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-		
-		System.out.print("Room number: ");
-		int roomNumber = sc.nextInt();
-		
-		System.out.print("Checkin date: ");
-		LocalDate checkin = LocalDate.parse(sc.next(), fmt1);
-		
-		System.out.print("Checkout date: ");
-		LocalDate checkout = LocalDate.parse(sc.next(), fmt1);
-		
-		// A DATA DE CHECKOUT NÃO PODE VIR ANTES DA DE CHECKIN (EXCEPTIONS)
-		Reservation reservation;
-		if (!checkout.isAfter(checkin)) {
-			System.err.println("A data de checkout não pode vir antes do checkin!");
-			reservation = new Reservation();
-		}
-		else {
-			reservation = new Reservation(roomNumber, checkin, checkout);
+		try {
+			Locale.setDefault(Locale.US);
+			Scanner sc = new Scanner (System.in);
+			DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			
+			// Leitura dos dados da reserva
+			System.out.print("Room number: ");
+			int roomNumber = sc.nextInt();
+			System.out.print("Checkin date: ");
+			LocalDate checkin = LocalDate.parse(sc.next(), fmt1);
+			System.out.print("Checkout date: ");
+			LocalDate checkout = LocalDate.parse(sc.next(), fmt1);
+			
+			// A DATA DE CHECKOUT NÃO PODE VIR ANTES DA DE CHECKIN (EXCEPTIONS)
+			Reservation reservation = new Reservation(roomNumber, checkin, checkout);
 			System.out.println(reservation.toString());
-		}
-
-		System.out.println();
-		System.out.println("---------------------UPDATED DATE------------------------------");
-		System.out.println();
-		
-		System.out.println("Enter date to update the reservation:");
-		
-		System.out.print("New checkin: ");
-		LocalDate newCheckinDate = LocalDate.parse(sc.next(), fmt1);
-		
-		System.out.print("New checkout: ");
-		LocalDate newCheckoutDate = LocalDate.parse(sc.next(), fmt1);
-		
-		// NÃO PODE INSERIR DATAS ANTES DA ATUAL (EXCEPTIONS)
-		// A DATA DE CHECKOUT NÃO PODE VIR ANTES DA DE CHECKIN (EXCEPTIONS)
-		
-		String error = reservation.updateDates(newCheckinDate, newCheckoutDate);
-		if (error != null) {
-			System.out.println("Error in reservation: " + error);
-		}
-		else {
-			System.out.println("Reservation: " + reservation.toString());
-		}
+	
+	
+			System.out.println();
+			System.out.println("---------------------UPDATED DATE----------------------------");
+			System.out.println();
+			
+			System.out.println("Enter date to update the reservation:");
+			sc.nextLine();
+			
+			// Atualização dos dados da reserva
+			System.out.print("New checkin: ");
+			LocalDate newCheckinDate = LocalDate.parse(sc.next(), fmt1);
+			System.out.print("New checkout: ");
+			LocalDate newCheckoutDate = LocalDate.parse(sc.next(), fmt1);
+			
+			// NÃO PODE INSERIR DATAS ANTES DA ATUAL (EXCEPTIONS)
+			// A DATA DE CHECKOUT NÃO PODE VIR ANTES DA DE CHECKIN (EXCEPTIONS)
+			
+			reservation.updateDates(newCheckinDate, newCheckoutDate);
+			System.out.println(reservation.toString());
 				
-		sc.close();
-
+				
+			sc.close();
+		}
+		catch (RuntimeException e) {
+		System.out.println(e.getMessage());
+		}
+		/*catch (ReservationException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		catch (ReservationUpdateDatesException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		catch (InputMismatchException e) {
+			System.out.println("Entrada inesperada!");
+		}
+		catch (DateTimeParseException e) {
+			System.out.println("Entrada inesperada!");
+		}*/
+		
 	}
-
+				
 }

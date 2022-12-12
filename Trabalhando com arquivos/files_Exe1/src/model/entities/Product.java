c33ba7c772bc592dba16ba20b0f435323c8f74e8
Marcos André <marcosdev2002@gmail.com>
2022-12-12 14:47:59 -0300

    Criando classe Product

---
+++ Trabalhando com arquivos/files_Exe1/src/model/entities/Product.java
@@ -0,0 +1,59 @@
+package model.entities;
+
+public class Product {
+	
+	private String name;
+	private Double price;
+	private Double quantity;
+	
+	// Construtores da classe
+	
+	public Product () {
+	}
+	
+	public Product (String name, double price, double quantity) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	// Métodos getters and setters
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Double getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Double quantity) {
+		this.quantity = quantity;
+	}
+	
+	// Métodos da classe
+	
+	public double totalValue () {
+		return this.price * this.quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name + ","
+				+ this.price + ","
+				+ this.quantity;
+	}
+
+}

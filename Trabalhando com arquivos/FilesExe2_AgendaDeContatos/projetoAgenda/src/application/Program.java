0544c1cd9332a3aef9f2d12bd246eba32e4fb9a8
Marcos André <marcosdev2002@gmail.com>
2022-12-13 16:02:34 -0300

    Criando método para introdução da interface no console

--- Trabalhando com arquivos/FilesExe2_AgendaDeContatos/projetoAgenda/src/application/Program.java
+++ Trabalhando com arquivos/FilesExe2_AgendaDeContatos/projetoAgenda/src/application/Program.java
@@ -1,11 +1,33 @@
 package application;
 
+import java.util.Scanner;
+
+import entities.ContactBook;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		
+		Phone phone = new Phone(new ContactBook(), introduction());
 
+	}
+	
+	public static int introduction () {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.print("**************** WELCOME TO YOUR CONTACT BOOK! ****************\n");
+		System.out.println("Choose the options below to continue using the contact book:");
+		
+		int choiceInit;
+		
+		System.out.println("1: Acessar a agenda | 2: Adicionar contato | 3: Excluir contato | 4: Sair do aplicativo");
+		choiceInit = sc.nextInt();
+		
+		sc.close();
+		
+		return choiceInit;
+		
 	}
 
 }

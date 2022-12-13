8a0c59169f6d6f425f0b6b1634f00e31f1e8a8a7
Marcos André <marcosdev2002@gmail.com>
2022-12-13 17:45:09 -0300

    Alterando o objeto Scanner para um atributo de classe

--- Trabalhando com arquivos/FilesExe2_AgendaDeContatos/projetoAgenda/src/application/Program.java
+++ Trabalhando com arquivos/FilesExe2_AgendaDeContatos/projetoAgenda/src/application/Program.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import entities.ContactBook;
 
 public class Program {
+	
+	private static Scanner input = new Scanner (System.in);
 
 	public static void main(String[] args) {
 		
@@ -14,14 +16,12 @@ public class Program {
 	}
 	
 	public static int introduction () {
-		
-		Scanner sc = new Scanner (System.in);
-		
+	
 		System.out.print("**************** WELCOME TO YOUR CONTACT BOOK! ****************\n");
 		System.out.println("Choose the options below to continue using the contact book:");
 		System.out.print("1: Access agenda | 2: Add contact | 3: Delete contact | 4: Deleted contacts | 5: Exit the application -> ");
 		
-		return sc.nextInt();
+		return input.nextInt();
 		
 	}
 
